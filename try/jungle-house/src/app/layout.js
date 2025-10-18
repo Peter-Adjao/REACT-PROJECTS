d82250300a0c9af8f6e65c79d82558ae71147f9d
Peter Adjao <pa376499@gmail.com>
2025-10-18 21:24:46 +0000

    feat: build Jungle House, an ecommerce Next.js app with reusable components and currency formatter

---
+++ try/jungle-house/src/app/layout.js
@@ -0,0 +1,16 @@
+
+
+
+export const metadata = {
+    title: "Jungle House",
+    descriptiton: "An ecommerce app to for all plants"
+};
+
+export default function RootLayout({ children}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+
+        </html>
+    )
+}
---
+++ try/jungle-house/src/app/page.js
@@ -0,0 +1,12 @@
+import Banner from "../components/Banner";
+import Cart from "../components/Cart";
+import "../styles/globals.css";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Banner />
+            <Cart />
+        </div>
+    );
+}
